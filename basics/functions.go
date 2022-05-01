edb3806110ca939d9637cc12696ec0d465d38d60
Webber <webber@takken.io>
2022-05-01 22:46:49 +0200

    lesson #10

--- basics/functions.go
+++ basics/functions.go
@@ -3,22 +3,31 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 func functions() {
-	names := []string{"visitor1", "visitor2"}
-	cycleNames(names, sayGreeting)
-	cycleNames(names, sayBye)
+	fullNames := []string{"huey duck", "dewey duck", "louie duck"}
 
+	// passing functions
+	cycleNames(fullNames, sayGreeting)
+	cycleNames(fullNames, sayBye)
+
+	// return types
 	radius := 12.0
 	area := circleArea(radius)
 	fmt.Printf("The area of a circle with radius %.1f is %.2f.\n", radius, area)
+
+	// multiple return values
+	for _, fullName := range fullNames {
+		firstInitial, secondInitial := getInitials(fullName)
+		fmt.Println(firstInitial, secondInitial)
+	}
 }
 
 func sayGreeting(name string) {
 	fmt.Printf("Good morning %s.\n", name)
 }
-
 func sayBye(name string) {
 	fmt.Printf("Good bye %s.\n", name)
 }
@@ -31,4 +40,20 @@ func cycleNames(names []string, fn func(string)) {
 
 func circleArea(radius float64) float64 {
 	return math.Pi * radius * radius
+}
+
+func getInitials(fullName string) (string, string) {
+	upper := strings.ToUpper(fullName)
+	names := strings.Split(upper, " ")
+
+	var initials []string
+	for _, name := range names {
+		initials = append(initials, name[:1])
+	}
+
+	if len(initials) > 1 {
+		return initials[0], initials[len(initials)-1]
+	}
+
+	return initials[0], "_"
 }
