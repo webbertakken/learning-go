0063b1d852f3a563e76ecb18c0180c2b73613295
Webber <webber@takken.io>
2022-05-04 22:36:30 +0200

    exercise 15 - functions

--- zero-to-mastery/lectures/demo/functions/functions.go
+++ zero-to-mastery/lectures/demo/functions/functions.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func double(x int) int {
+	return x * 2
+}
+
+func add(first, second int) int {
+	return first + second
+}
+
+func greet() {
+	fmt.Println("Hello from greet function.")
+}
+
+func main() {
+	greet()
+	dozen := double(6)
+	fmt.Println("A dozen is", dozen)
+
+	bakersDozen := add(dozen, 1)
+	fmt.Println("A baker's dozen is", bakersDozen)
+
+	anotherBakersDozen := add(double(6), 1)
+	fmt.Println("Another baker's dozen is", anotherBakersDozen)
 }
