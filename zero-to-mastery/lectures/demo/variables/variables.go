d701fc1b3a5fd133bf2155a1b7a32a821c55c735
Webber <webber@takken.io>
2022-05-04 22:18:42 +0200

    exercise 12 - variables

--- zero-to-mastery/lectures/demo/variables/variables.go
+++ zero-to-mastery/lectures/demo/variables/variables.go
@@ -3,4 +3,36 @@ package main
 import "fmt"
 
 func main() {
+	var myName = "Webber"
+	fmt.Println("My name is", myName)
+
+	var name string = "Kathy"
+	fmt.Println("name =", name)
+
+	userName := "admin"
+	fmt.Println("username =", userName)
+
+	var sum int
+	fmt.Println("The sum is", sum)
+
+	part1, other := 1, 5
+	fmt.Println("Part 1 is", part1, "other is", other)
+
+	// Ok, error idiom - can reassign 2nd variable, even though it's already created before
+	part2, other := 2, 0
+	fmt.Println("Part 2 is", part2, "other is", other)
+
+	sum = part1 + part2
+	fmt.Println("sum =", sum)
+
+	var (
+		lessonName = "Variables"
+		lessonType = "Demo"
+	)
+
+	fmt.Println("lessonName=", lessonName)
+	fmt.Println("lessonType=", lessonType)
+
+	word1, word2, _ := "hello", "world", "!"
+	fmt.Println(word1, word2)
 }
