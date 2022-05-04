bf8f94234668076d02e5f6b70f1fc3465d359490
Webber <webber@takken.io>
2022-05-04 23:58:52 +0200

    exercise 19 - if-else

--- zero-to-mastery/lectures/demo/if-else/if-else.go
+++ zero-to-mastery/lectures/demo/if-else/if-else.go
@@ -7,6 +7,19 @@ func average(a, b, c int) float32 {
 	return float32(a+b+c) / 3
 }
 
+//goland:noinspection GoBoolExpressions
 func main() {
+	quiz1, quiz2, quiz3 := 9, 7, 8
 
+	if quiz1 > quiz2 {
+		fmt.Println("quiz1 scored higher than quiz2.")
+	} else if quiz2 < quiz1 {
+		fmt.Println("quiz2 scored higher than quiz1.")
+	} else {
+		fmt.Println("quiz1 and quiz2 scored equally.")
+	}
+
+	if average(quiz1, quiz2, quiz3) > 7 {
+		fmt.Println("Noice grades overall.")
+	}
 }
