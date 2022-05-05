3f8dfef9b7f37dd820482aebf1ef47822a45a978
Webber <webber@takken.io>
2022-05-06 01:36:00 +0200

    exercise 27 - dice

--- zero-to-mastery/lectures/exercise/sr-dice/dice.go
+++ zero-to-mastery/lectures/exercise/sr-dice/dice.go
@@ -23,7 +23,37 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func diceRoll(sides int) int {
+	return int(rand.Int31n(int32(sides)) + 1)
+}
+
+func comboRoll(dice int, sides int, times int) {
+	for i := 0; i < times; i++ {
+
+		// Had to use []any to keep it practical (trying to pass each element as a separate argument to fmt.Println().
+		// It seemed a bit much to make a helper function just to convert a slice of integers to strings.
+		// https://stackoverflow.com/questions/37532255/one-liner-to-transform-int-into-string
+
+		// I believe go language could be improved, unless I misunderstand something fundamental.
+		// Asked a question on discord, hopefully some experts on go will be able to say something smart about it.
+		// https://discord.com/channels/423464391791476747/933759051190370314/971905425572261919
+		var numbers []any
+
+		for j := 0; j < dice; j++ {
+			numbers = append(numbers, diceRoll(sides))
+		}
+
+		fmt.Println(numbers...)
+	}
+}
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+	comboRoll(3, 6, 3)
 }
