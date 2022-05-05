a4b3f5f551ed2211a88cd618f72fa776e9a847dd
Webber <webber@takken.io>
2022-05-05 23:55:16 +0200

    exercise 25 - loops

--- zero-to-mastery/lectures/demo/loops/loops.go
+++ zero-to-mastery/lectures/demo/loops/loops.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
-func main() {
+func fibonacci(maxNumber int) {
+	fmt.Println("Sum is", 1)
+	for i, j := 0, 1; i <= maxNumber; {
+		sum := i + j
 
+		fmt.Println("Sum is", sum)
+
+		i = j
+		j = sum
+	}
+}
+
+func main() {
+	fibonacci(900)
+
+	sum := 55
+	for sum > 10 {
+		sum -= 5
+		fmt.Println("Subtract 5. Sum is now", sum)
+	}
 }
