ae381c98a906b9351af53d2ec0a9c4998cab77b9
Webber <webber@takken.io>
2022-05-06 21:19:59 +0200

    exercise 33 - arrays

--- zero-to-mastery/lectures/demo/arrays/arrays.go
+++ zero-to-mastery/lectures/demo/arrays/arrays.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+type Room struct {
+	Name    string
+	Cleaned bool
+}
+
+func checkCleanliness(rooms [4]Room) {
+	for i := 0; i < len(rooms); i++ {
+		room := rooms[i]
+		if room.Cleaned {
+			fmt.Println("Room is clean", room.Name)
+		} else {
+			fmt.Println("Room is diry", room.Name)
+		}
+	}
+}
+
+func main() {
+	rooms := [...]Room{
+		{Name: "Office"},
+		{Name: "Warehouse"},
+		{Name: "Reception"},
+		{Name: "Ops"},
+	}
+
+	checkCleanliness(rooms)
+
+	fmt.Println("Perform cleaning...")
+	rooms[2].Cleaned = true
+	rooms[3].Cleaned = true
+
+	checkCleanliness(rooms)
 }
