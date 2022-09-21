3009c5671c8f14ec6613392039143c259bb0d7ce
Webber <webber@takken.io>
2022-09-22 00:07:03 +0200

    exercise 44 - pointers

--- zero-to-mastery/lectures/exercise/pointers/pointers.go
+++ zero-to-mastery/lectures/exercise/pointers/pointers.go
@@ -3,21 +3,60 @@
 //  on products.
 //
 //--Requirements:
-//* Create a structure to store items and their security tag state
-//  - Security tags have two states: active (true) and inactive (false)
-//* Create functions to activate and deactivate security tags using pointers
-//* Create a checkout() function which can deactivate all tags in a slice
-//* Perform the following:
-//  - Create at least 4 items, all with active security tags
-//  - Store them in a slice or array
-//  - Deactivate any one security tag in the array/slice
-//  - Call the checkout() function to deactivate all tags
-//  - Print out the array/slice after each change
 
 package main
 
 import "fmt"
 
+//* Create a structure to store items and their security tag tag
+//  - Security tags have two states: active (true) and inactive (false)
+
+const (
+	Active   = true
+	Inactive = false
+)
+
+type SecurityTag = bool
+
+type Product struct {
+	name string
+	tag  SecurityTag
+}
+
+//* Create functions to activate and deactivate security tags using pointers
+func activate(product *SecurityTag) {
+	*product = Active
+}
+
+func deactivate(product *SecurityTag) {
+	*product = Inactive
+}
+
+//* Create a checkout() function which can deactivate all tags in a slice
+func checkout(products []Product) {
+	for i := 0; i < len(products); i++ {
+		deactivate(&products[i].tag)
+	}
+}
+
 func main() {
+	//* Perform the following:
+	//  - Create at least 4 items, all with active security tags
+	foo := Product{"Foo", Active}
+	bar := Product{"Bar", Active}
+	baz := Product{"Baz", Active}
+	fuz := Product{"Fuz", Active}
+
+	//  - Store them in a slice or array
+	basket := []Product{foo, bar, baz, fuz}
+	fmt.Println(basket)
+
+	//  - Deactivate any one security tag in the array/slice
+	deactivate(&basket[1].tag)
+	fmt.Println(basket)
+
+	//  - Call the checkout() function to deactivate all tags
+	checkout(basket)
+	fmt.Println(basket)
 
 }
