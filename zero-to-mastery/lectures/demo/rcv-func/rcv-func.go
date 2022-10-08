2546d3b1153c266852fed78aafce3622630a5292
Webber <webber@takken.io>
2022-10-09 00:42:02 +0200

    exercise 49 - receiver functions

--- zero-to-mastery/lectures/demo/rcv-func/rcv-func.go
+++ zero-to-mastery/lectures/demo/rcv-func/rcv-func.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import "log"
+
+type Space struct {
+	occupied bool
+}
+
+type ParkingLot struct {
+	spaces []Space
+}
+
+func occupySpace(lot *ParkingLot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func (lot *ParkingLot) occupySpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = true
+}
+
+func vacateSpace(lot *ParkingLot, spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
+
+func (lot *ParkingLot) vacateSpace(spaceNum int) {
+	lot.spaces[spaceNum-1].occupied = false
+}
 
 func main() {
+	lot := ParkingLot{spaces: make([]Space, 3)}
+	log.Println("initial:", lot)
 
+	occupySpace(&lot, 2)
+	lot.occupySpace(1)
+	log.Println("after occupying some spaces:", lot)
+
+	lot.vacateSpace(2)
+	log.Println("after vacating space 2:", lot)
 }
