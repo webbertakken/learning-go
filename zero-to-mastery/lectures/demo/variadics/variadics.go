be0c5b1b5f1747f08b571114d97da13b77415eb5
Webber <webber@takken.io>
2022-10-09 01:15:53 +0200

    exercise 53 variadics

--- zero-to-mastery/lectures/demo/variadics/variadics.go
+++ zero-to-mastery/lectures/demo/variadics/variadics.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
+func sum(numbers ...int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
 
+	return sum
+}
+
+func main() {
+	a := []int{1, 2, 3}
+	b := []int{4, 5, 6}
+
+	all := append(a, b...)
+
+	answer := sum(all...)
+	fmt.Println(answer)
 }
