e9e91c2172e6a5099c98213bb11d0381e8bbccd3
Webber <webber@takken.io>
2022-10-09 15:19:02 +0200

    exercise 62 interface

--- zero-to-mastery/lectures/exercise/interfaces/interfaces.go
+++ zero-to-mastery/lectures/exercise/interfaces/interfaces.go
@@ -3,12 +3,7 @@
 //  to the correct vehicle lift, based on vehicle size.
 //
 //--Requirements:
-//* The shop has lifts for multiple vehicle sizes/types:
-//  - Motorcycles: small lifts
-//  - Cars: standard lifts
-//  - Trucks: large lifts
-//* Write a single function to handle all of the vehicles
-//  that the shop works on.
+
 //* Vehicles have a model name in addition to the vehicle type:
 //  - Example: "Truck" is the vehicle type, "Road Devourer" is a model name
 //* Direct at least 1 of each vehicle type to the correct
@@ -21,6 +16,86 @@ package main
 
 import "fmt"
 
-func main() {
+//--Requirements:
+//* The shop has lifts for multiple vehicle sizes/types:
+//  - Motorcycles: small lifts
+//  - Cars: standard lifts
+//  - Trucks: large lifts
 
+type Lift int
+
+const (
+	SmallLift Lift = iota
+	StandardLift
+	LargeLift
+)
+
+type LiftDirector interface {
+	PickLift() Lift
+}
+
+type Motorcycle struct {
+	make  string
+	model string
+}
+
+type Car struct {
+	make  string
+	model string
+}
+
+type Truck struct {
+	make  string
+	model string
+}
+
+func (m Motorcycle) String() string {
+	return fmt.Sprintf("Motorcycle: %v, %v", m.make, m.model)
+}
+
+func (m Motorcycle) PickLift() Lift {
+	return SmallLift
+}
+
+func (c Car) String() string {
+	return fmt.Sprintf("Car: %v, %v", c.make, c.model)
+}
+
+func (c Car) PickLift() Lift {
+	return StandardLift
+}
+
+func (t Truck) String() string {
+	return fmt.Sprintf("Truck: %v, %v", t.make, t.model)
+}
+
+func (t Truck) PickLift() Lift {
+	return LargeLift
+}
+
+//* Write a single function to handle all of the vehicles
+//  that the shop works on.
+func MoveToLift(d LiftDirector) {
+	switch d.PickLift() {
+	case SmallLift:
+		fmt.Printf("send %v to small lift\n", d)
+	case StandardLift:
+		fmt.Printf("send %v to standard lift\n", d)
+	case LargeLift:
+		fmt.Printf("send %v to large lift\n", d)
+	}
+}
+
+type Lifts struct {
+	motorcycleLift Motorcycle
+	carLift        Car
+	truckLift      Truck
+}
+
+func main() {
+	incomingVehicles := []LiftDirector{Car{"Mazda", "323"}, Motorcycle{"Kawasaki", "Ninja 400"}, Truck{"IVECO", "Daily 4x4"}}
+
+	for _, vehicle := range incomingVehicles {
+		MoveToLift(vehicle)
+	}
 }
