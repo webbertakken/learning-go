0540da1eb725bb00c3e1d4a9192f1897b4f96b72
Webber <webber@takken.io>
2022-10-09 16:43:42 +0200

    exercise 66 readers

--- zero-to-mastery/lectures/demo/readers/readers.go
+++ zero-to-mastery/lectures/demo/readers/readers.go
@@ -1,8 +1,40 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	r := bufio.NewReader(os.Stdin)
+
+	sum := 0
+	for {
+		input, inputErr := r.ReadString('\n')
+		if inputErr == io.EOF {
+			break
+		}
+		if inputErr != nil {
+			fmt.Println("Error reading Stdin", inputErr)
+		}
+
+		n := strings.Trim(input, " \r\n")
+		if n == "" {
+			continue
+		}
+
+		num, convertErr := strconv.Atoi(n)
+		if convertErr != nil {
+			fmt.Printf("Error converting \"%v\" to a number\n", n)
+			continue
+		}
+
+		sum += num
+		fmt.Println("sum:", sum)
+		fmt.Print("input: ")
+	}
 }
