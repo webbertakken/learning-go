5436799b68d1a56ba1ea14516cd77a8254231860
Webber <webber@takken.io>
2022-10-09 02:27:17 +0200

    exercise 59 testing

--- zero-to-mastery/lectures/exercise/testing/rcv-func_test.go
+++ zero-to-mastery/lectures/exercise/testing/rcv-func_test.go
@@ -1,3 +1,7 @@
+package main
+
+import "testing"
+
 //--Summary:
 //  Copy your rcv-func solution to this directory and write unit tests.
 //
@@ -10,3 +14,72 @@
 //
 //--Notes:
 //* Use `go test -v ./exercise/testing` to run these specific tests
+
+func newPlayer() Player {
+	return Player{
+		"Player",
+		100,
+		100,
+		0,
+		30,
+	}
+}
+
+func TestHealth(t *testing.T) {
+	player := newPlayer()
+
+	if player.Health != 100 {
+		t.Errorf("Expected player to start with 100 health, got %v", player.Health)
+	}
+
+	if player.MaxHealth != 100 {
+		t.Errorf("Expected player to have 100 maxHealth, got %v", player.MaxHealth)
+	}
+
+	player.takeDamage(10)
+	if player.Health != 90 {
+		t.Fatalf("Expected player to take damage")
+	}
+
+	player.heal(10)
+	if player.Health != 100 {
+		t.Fatalf("Expected player to not heal above 100%%")
+	}
+
+	player.takeDamage(player.MaxHealth + 1)
+	if player.Health != 0 {
+		t.Fatalf("Expected player to not drop below 0 health, got %v", player.Health)
+	}
+}
+
+func TestEnergy(t *testing.T) {
+	player := newPlayer()
+
+	if player.Energy != 0 {
+		t.Errorf("Expected player to start with 0 energy, got %v", player.Energy)
+	}
+
+	if player.MaxEnergy != 30 {
+		t.Errorf("Expected player to have 30 maxEnergy, got %v", player.MaxEnergy)
+	}
+
+	player.recharge(10)
+	if player.Energy != 10 {
+		t.Fatalf("Expected player to recharge energy")
+	}
+
+	player.useEnergy(5)
+	if player.Energy != 5 {
+		t.Fatalf("Expected player to use energy")
+	}
+
+	player.recharge(player.MaxEnergy + 1)
+	if player.Energy != 30 {
+		t.Fatalf("Expected player energy not to go about maxEnergy, got %v", player.Energy)
+	}
+
+	player.useEnergy(player.MaxEnergy + 1)
+	if player.Energy != 0 {
+		t.Fatalf("Expected player energy to not go into deficit, got %v", player.Energy)
+	}
+}
