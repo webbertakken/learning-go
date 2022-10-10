2f02147a83a1f19ec7387cefa7d0467f9b716e76
Webber <webber@takken.io>
2022-10-11 00:19:26 +0200

    exercise 89 synchronisation

--- zero-to-mastery/lectures/exercise/sync/sync.go
+++ zero-to-mastery/lectures/exercise/sync/sync.go
@@ -2,6 +2,18 @@
 //  Create a program that can read text from standard input and count the
 //  number of letters present in the input.
 //
+
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+	"sync"
+	"unicode"
+)
+
 //--Requirements:
 //* Count the total number of letters in any chosen input
 //* The input must be supplied from standard input
@@ -16,10 +28,54 @@
 //* Use any synchronization techniques to implement the program:
 //  - Channels / mutexes / wait groups
 
-package main
+type Counter struct {
+	sync.Mutex
+	letters int
+}
 
-import (
-	"fmt"
-)
+func (c *Counter) Add(amount int) {
+	c.Lock()
+	defer c.Unlock()
+	c.letters += amount
+}
 
-func main() {}
+func (c *Counter) Results() {
+	log.Printf("%v letters\n", c.letters)
+}
+
+func (c *Counter) Reset() {
+	c.letters = 0
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		value := scanner.Text()
+		words := strings.Split(value, " ")
+
+		counter := Counter{}
+		group := sync.WaitGroup{}
+		for index, word := range words {
+			group.Add(1)
+			go analyseWord(index, &group, &counter, word)
+		}
+
+		group.Wait()
+		counter.Results()
+		counter.Reset()
+	}
+}
+
+func analyseWord(index int, group *sync.WaitGroup, counter *Counter, word string) {
+	defer group.Done()
+
+	count := 0
+	for _, character := range word {
+		if unicode.IsLetter(character) {
+			count += 1
+		}
+	}
+
+	counter.Add(count)
+}
