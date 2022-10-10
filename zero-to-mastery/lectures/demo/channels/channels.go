710bc76733e57ff56e431d2f7e14045ee5e035f0
Webber <webber@takken.io>
2022-10-10 20:01:47 +0200

    exercise 84 channels

--- zero-to-mastery/lectures/demo/channels/channels.go
+++ zero-to-mastery/lectures/demo/channels/channels.go
@@ -1,5 +1,64 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+type ControlMsg int
+
+const (
+	DoExit = iota
+	ExitOk
+)
+
+type Job struct {
+	data int
+}
+
+type Result struct {
+	result int
+	job    Job
+}
+
+func doubler(jobs <-chan Job, results chan<- Result, control chan ControlMsg) {
+	for {
+		select {
+		case msg := <-control:
+			switch msg {
+			case DoExit:
+				fmt.Println("Exit goroutine")
+				control <- ExitOk
+				return
+			default:
+				panic(fmt.Sprintf("Unhandled control message %v", msg))
+			}
+		case job := <-jobs:
+			results <- Result{result: job.data * 2, job: job}
+		}
+	}
+}
+
+func main() {
+	jobs := make(chan Job, 50)
+	results := make(chan Result, 50)
+	control := make(chan ControlMsg)
+
+	go doubler(jobs, results, control)
+
+	for i := 0; i < 30; i++ {
+		jobs <- Job{i}
+	}
+
+	for {
+		select {
+		case result := <-results:
+			fmt.Println(result)
+		case <-time.After(500 * time.Millisecond):
+			control <- DoExit
+			<-control
+			fmt.Println("Program exiting")
+			return
+		}
+	}
 }
