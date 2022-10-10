6cfc9d6408ca3a2c2cebcb6d15da6ade263927c0
Webber <webber@takken.io>
2022-10-10 16:18:04 +0200

    exercise 76 function literals

--- zero-to-mastery/lectures/demo/func-literal/func-literal.go
+++ zero-to-mastery/lectures/demo/func-literal/func-literal.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import "fmt"
 
+func add(a, b int) int {
+  return a + b
+}
+
+func subtract(a, b int) int {
+  return a - b
+}
+
+type OperatorFn func(a, b int) int
+
+func compute(a, b int, operatorFn OperatorFn) int {
+  fmt.Printf("Running a computation with %v and %v\n", a, b)
+  return operatorFn(a, b)
+}
+
+func main() {
+  fmt.Println("3 + 3 =", compute(3, 3, add))
+  fmt.Println("3 - 3 =", compute(3, 3, subtract))
+  fmt.Println("3 / 3 =", compute(3, 3, func(a, b int) int { return a / b }))
+
+  mul := func(a, b int) int { return a * b }
+  fmt.Println("3 * 3 =", compute(3, 3, mul))
 }
