98f3d4edff298821889b15a9c7cd6d248b0058f0
Webber <webber@takken.io>
2022-10-10 17:31:32 +0200

    exercise 81 goroutines

--- zero-to-mastery/lectures/demo/goroutines/goroutines.go
+++ zero-to-mastery/lectures/demo/goroutines/goroutines.go
@@ -1,9 +1,36 @@
 package main
 
-import "fmt"
-import "time"
-import "unicode"
+import (
+	"fmt"
+	"log"
+	"time"
+	"unicode"
+)
 
 func main() {
 	data := []rune{'a', 'b', 'c', 'd'}
+
+	var capitalised []rune
+
+	capitalise := func(r rune) {
+		capitalisedCharacter := unicode.ToUpper(r)
+		capitalised = append(capitalised, capitalisedCharacter)
+		log.Printf("%c", capitalisedCharacter)
+	}
+
+	// Note that like this, write access to `capitalised` variable is not mutually exclusive and CHARACTERS GO MISSING.
+	// Later in the course, synchronization with Mutex, communication via channels, and WaitGroups are all discussed which
+	// solve the issue encountered in this demo.
+
+	for _, character := range data {
+		go capitalise(character)
+	}
+
+	for i := 0; i < len(data); i++ {
+		go capitalise(data[i])
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Printf("Capitalised: %c\n", capitalised)
 }
