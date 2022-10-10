b9f9b20867c7a1cb56212be16e43a66f27479b05
Webber <webber@takken.io>
2022-10-10 21:16:50 +0200

    exercise 88 mutexes

--- zero-to-mastery/lectures/demo/mutexes/mutexes.go
+++ zero-to-mastery/lectures/demo/mutexes/mutexes.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -10,6 +11,34 @@ func wait() {
 	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
 }
 
+type Hits struct {
+	count int
+	sync.Mutex
+}
+
+func serve(wg *sync.WaitGroup, hits *Hits, iteration int) {
+	defer wg.Done()
+
+	wait()
+
+	hits.Lock()
+	defer hits.Unlock()
+
+	hits.count += 1
+	fmt.Printf("%04d: done\n", iteration)
+}
+
 func main() {
 	rand.Seed(time.Now().UnixNano())
+
+	group := sync.WaitGroup{}
+	hitCounter := Hits{}
+
+	for i := 0; i < 100; i++ {
+		group.Add(1)
+		go serve(&group, &hitCounter, i)
+	}
+
+	group.Wait()
+	fmt.Printf("Total hits: %v", hitCounter.count)
 }
