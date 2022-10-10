292f77d793522677f9d6103afa1fa5e78902e528
Webber <webber@takken.io>
2022-10-10 21:06:16 +0200

    exercise 87 waitgroups

--- zero-to-mastery/lectures/demo/waitgroups/waitgroups.go
+++ zero-to-mastery/lectures/demo/waitgroups/waitgroups.go
@@ -1,11 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "math/rand"
-    "sync"
-    "time"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	wg := sync.WaitGroup{}
+
+	counter := 0
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		counter += 1
+
+		go func(i int) {
+			defer func() {
+				fmt.Printf("%v groups left to complete\n", counter)
+				counter -= 1
+				wg.Done()
+			}()
+
+			duration := time.Duration(rand.Intn(500) * int(time.Millisecond))
+			fmt.Printf("%02d: Waiting for %v\n", i, duration)
+			time.Sleep(duration)
+		}(i)
+	}
+
+	fmt.Printf("Waiting for goroutines to all finish.\n")
+	wg.Wait()
+	fmt.Printf("Done!\n")
 }
